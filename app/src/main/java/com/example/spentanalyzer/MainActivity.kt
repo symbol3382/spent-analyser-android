51ea13290dc703635b97cd68c1937a010d45403b
Gourav Verma <symbol@Gouravs-Mac-mini.local>
2022-11-22 01:56:34 +0530

    Added Navigation to sign in button
    Updated Edit text background style to rounded

--- app/src/main/java/com/example/spentanalyzer/MainActivity.kt
+++ app/src/main/java/com/example/spentanalyzer/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.spentanalyzer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.spentanalyzer.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
     }
 }
